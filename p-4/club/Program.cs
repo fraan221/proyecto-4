4b4bbb6ca66b467e345960e25fc69cefefc9ce2f
unknown <franhe345@gmail.com>
2024-10-16 16:03:39 -0300

    first version

---
+++ p-4/club/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: fraan
+ * Date: 10/16/2024
+ * Time: 4:02 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace club
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
